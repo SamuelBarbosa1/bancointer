1cc5733ae900a2354e03502b3edc70f93ff58a47
Samuel <samuelbarbosa2001oliveira@gmail.com>
2023-12-28 23:06:55 -0300

    novas função

---
+++ src/components/Header/Gradient/animations.ts
@@ -0,0 +1,12 @@
+export const GRADIENT_ANIMATION = {
+    unMounted: { width: '0%' },
+    mounted: {
+      width: '100%',
+      transition: {
+        delay: 0.5,
+        duration: 1.8,
+        ease: 'easeInOut',
+      },
+    },
+  };
+  
